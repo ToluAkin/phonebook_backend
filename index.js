6b91d6abc6be335b7cae7ff90010bdf732d0b1ef
Toluwalase <tolu@cottacush.com>
2021-06-11 09:22:07 +0100

    deploy script added

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(morgan('dev'))
 // serve static files like html
 app.use(express.static('build'))
 
-//e40YytB80ebguNLU
+// app.use(requestLogger)
 
 // let persons = [
 //     {
@@ -52,9 +52,11 @@ app.get('/api/persons', (req, res) => {
 
 //Display a page
 app.get('/info', (req, res) => {
-    const text = `<div><p>Phonebook info has ${persons.length} people</p>
+    Person.find({}).then(persons => {
+        const text = `<div><p>Phonebook info has ${persons.length} people</p>
                  <p>${new Date}</p></div>`
-    res.send(text)
+        res.send(text)
+    })
 })
 
 //get person by id
@@ -67,18 +69,17 @@ app.get('/api/persons/:id', (req, res) => {
             } else {
                 res.status(404).end()
             }
-        }).catch(error => {
-            console.log(error)
-            res.status(400).send({ error: 'malformatted id' })
-        })
+        }).catch(error => next(error))
 })
 
 //delete by id
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-    
-   res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    const id = req.params.id
+    Person.findByIdAndRemove(id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch (error => next(error))
 })
 
 //add person to the phonebook
@@ -109,6 +110,28 @@ app.post('/api/persons', (req, res) => {
         // }
     } 
 })
+
+
+/**
+ * checks if the error is a CastError exception
+ * @param {*} error 
+ * @param {*} request 
+ * @param {*} response 
+ * @param {*} next 
+ * @returns 
+ */
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 //Setting a port for the application
 const PORT = process.env.PORT
